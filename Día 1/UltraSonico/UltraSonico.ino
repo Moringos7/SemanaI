8fc29eb3ad435ac070870c8720fec194e61e607f
Moringos7 <moringos7@gmail.com>
2019-10-29 11:13:44 -0600

    Cambios Ultrasonico

--- Día 1/UltraSonico/UltraSonico.ino
+++ Día 1/UltraSonico/UltraSonico.ino
@@ -1,3 +1,10 @@
+/*#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>
+#define FIREBASE_HOST "semanai-4ea99.firebaseio.com"
+#define FIREBASE_AUTH "az4SQ6BPBqScFTFzeciectZz71Axmmc6XlDvEyHJ"
+#define WIFI_SSID "Tec-IoT"
+#define WIFI_PASSWORD "spotless.magnetic.bridge"*/
+
 int disparador = 2;
 int entrada = 0;
 
@@ -5,6 +12,7 @@ void setup(){
   pinMode(disparador,OUTPUT);
   pinMode(entrada,INPUT);
   Serial.begin(9600);
+  Serial.println("Hola");
 }
 
 void loop(){
@@ -19,5 +27,6 @@ void loop(){
   distancia = float(tiempo * 0.0343);
 
   Serial.println(distancia);
+  
   delay(300);
 }
