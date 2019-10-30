b635cbf03fc751d78000010756eca956b63b528a
Moringos7 <moringos7@gmail.com>
2019-10-30 12:57:19 -0600

    Codigo Arduino Envio datos

--- Día 2/Reto 2/3Sensores1LCD/3Sensores1LCD.ino
+++ Día 2/Reto 2/3Sensores1LCD/3Sensores1LCD.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
-#define FIREBASE_HOST "reto-2-semana-i.firebaseio.com"
-#define FIREBASE_AUTH "jlxVNTxKAmwMtP1lcQMGzXnkjJuZtpohEcezGxvm"
+#define FIREBASE_HOST "semanai-4ea99.firebaseio.com"
+#define FIREBASE_AUTH "az4SQ6BPBqScFTFzeciectZz71Axmmc6XlDvEyHJ"
 #define WIFI_SSID "Tec-IoT"
 #define WIFI_PASSWORD "spotless.magnetic.bridge"
 
@@ -13,7 +13,7 @@
 //Libreria Humedad
 #include "DHT.h"
 #define DHTTYPE DHT11
-#define dht_dpin 15
+#define dht_dpin 12
 DHT dht(dht_dpin, DHTTYPE);
 
 //Sensor del pir
@@ -30,7 +30,7 @@ int entrada = 14;
 //Se crea el objeto LCD direccion en la direccion de memoria y en columas y filas
 LiquidCrystal_I2C lcd(0x3F, 20,4);
 
-void setup() {
+void setup() { 
   //Ultrasonico Declaracion
   pinMode(disparador,OUTPUT);
   pinMode(entrada,INPUT);
@@ -62,14 +62,41 @@ void setup() {
 }
 
 void loop() {
+  ///// Generación y envio de datos
+  //Movimiento
+  long statePir = digitalRead(sensorPin);
+  Firebase.setInt("Movimiento",statePir);
+  delay(90);
+  //Temperatura y Humedad
+  float humedad = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+  Firebase.setFloat("Humedad",humedad);
+  Firebase.setFloat("Temperatura",temperatura);
+  delay(90);
+  //Luz
+  int luz=analogRead(A0);
+  luz=map(luz,650,10,100,0);
+  Firebase.setInt("Luz",luz);
+  delay(90);
+  //Distancia
+  long tiempo;
+  float distancia;
+  digitalWrite(disparador,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(disparador,LOW);
+  tiempo = (pulseIn(entrada,HIGH)/2);
+  distancia = float(tiempo * 0.0343);
+  Firebase.setFloat("Distancia",distancia);
+  //
+  delay(90);
+  lcd.clear();
   if(digitalRead(botonPin) == 0){
     cont = cont + 1;
     cont = cont%4;
   }
   if(cont == 0){
     //Leer el estado del Movimiento
-    long statePir = digitalRead(sensorPin);
-    Firebase.setInt("Movimiento",statePir);
+    
     if(statePir == LOW){
       lcd.print("No me muevo");
       }else{
@@ -77,39 +104,23 @@ void loop() {
       }
   }else if(cont == 1){
     //Leer la temperatura y humedad
-    float humedad = dht.readHumidity();
-    float temperatura = dht.readTemperature();
+   
     //Imprimir la humedad y temperatura
     lcd.print("Humedad = ");
     lcd.print(humedad);
-    Firebase.setFloat("Humedad",humedad);
+    
     lcd.setCursor(0,2);
     lcd.print("Temperatura = ");
     lcd.print(temperatura);
-    Firebase.setFloat("Temperatura",temperatura);
+    
   }else if(cont == 2){
-    int luz=analogRead(A0);
-    luz=map(luz,650,10,100,0);
-    Firebase.setInt("Luz",luz);
     if(luz > 100){
       lcd.print("Hay mucha luz");
     }else{
       lcd.print("No hay tanta luz");
     }
   }else if(cont == 3){
-    long tiempo;
-    float distancia;
-
-    digitalWrite(disparador,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(disparador,LOW);
-
-    tiempo = (pulseIn(entrada,HIGH)/2);
-    distancia = float(tiempo * 0.0343);
     lcd.print(distancia);
-    Firebase.setFloat("Distancia",distancia);
-    Serial.println(distancia);
+    //Serial.println(distancia);
   }
-  delay(350);
-  lcd.clear();
 }
