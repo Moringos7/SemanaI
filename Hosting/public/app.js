a980f02aed6f2f6ba0829eeb3b6fa69034c8b14c
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-11-01 10:14:39 -0600

    Proyecto funcional v1.0

--- Hosting/public/app.js
+++ Hosting/public/app.js
@@ -11,9 +11,19 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-//Getter de valores a través de los ID asignados
+//!Crear referencia a los elementos del HTMl
 var luz = document.getElementById("luz");
-var movimiento = document.getElementById("movimiento");
+
+var distanciaFrente = document.getElementById("distancia-frente");
+
+var distanciaAtras = document.getElementById("distancia-atras");
+
+var temperatura = document.getElementById("temperatura");
+
+
+var rele1 = document.getElementById("rele1");
+var rele2 = document.getElementById("rele2");
+
 var salir = document.getElementById("btnSalir");
 var correo = document.getElementById("inputEmail");
 var contraseña = document.getElementById("inputPassword");
@@ -60,6 +70,27 @@ auth.onAuthStateChanged(function(user) {
       .database()
       .ref()
       .child("Luz");
+
+
+    var fb_distanciaFrente = firebase
+    .database()
+    .ref()
+    .child("Frente");
+
+    var fb_distanciaAtras = firebase
+    .database()
+    .ref()
+    .child("Atras");
+
+    var fb_temperatura = firebase
+    .database()
+    .ref()
+    .child("Temperatura");
+
+    var fb_rele1 = firebase
+    .database()
+    .ref()
+    .child("Ventana");
     // var fb_red = firebase
     //   .database()
     //   .ref()
@@ -77,45 +108,87 @@ auth.onAuthStateChanged(function(user) {
     var t1 = 0;
 
     //Bloque de Botones - Reley's
-    fb_rele1.on("value", function(snapshot) {
-      /*Cambiará el aspecto del botón según el valor a través de las clases
-            para dar un aspecto de activo */
-      if (snapshot.val() == 1) {
-        $("#toogle1")
-          .removeClass("btn-default")
-          .addClass("btn-success");
-        t1 = 1;
-      } else {
-        $("#toogle1")
-          .removeClass("btn-success")
-          .addClass("btn-default");
-        t1 = 0;
-      }
-    });
+    // fb_rele1.on("value", function(snapshot) {
+    //   /*Cambiará el aspecto del botón según el valor a través de las clases
+    //         para dar un aspecto de activo */
+    //   if (snapshot.val() == 1) {
+    //     $("#toogle1")
+    //       .removeClass("btn-default")
+    //       .addClass("btn-success");
+    //     t1 = 1;
+    //   } else {
+    //     $("#toogle1")
+    //       .removeClass("btn-success")
+    //       .addClass("btn-default");
+    //     t1 = 0;
+    //   }
+    // });
     rele1.addEventListener("click", function() {
-      if (t1 == 1) fb_rele1.set(0);
-      else fb_rele1.set(1);
+      fb_rele1.set(1);
+    });
+
+    rele2.addEventListener("click", function() {
+      fb_rele1.set(0);
     });
 
     //Bloque card - Sensores
 
+    //!Setter en html que si funcionan
     fb_luz.on("value", function(snapshot) {
-      luz.innerHTML = snapshot.val().Luz + " % de luz";
-    });
+      var valLuz = snapshot.val();
 
-    fb_movimiento.on("value", function(snapshot) {
-      if (snapshot.val() == 1) {
-        movimiento.innerHTML = "Hay Movimiento";
-        $("#cardM")
-          .addClass("bg-info")
-          .addClass("text-white");
+      if (valLuz > 70) {
+        luz.innerHTML = "Luces Apagadas";
       } else {
-        movimiento.innerHTML = "No hay Movimiento";
-        $("#cardM")
-          .removeClass("bg-info")
-          .removeClass("text-white");
+        luz.innerHTML = "Luces Encendidas";
       }
     });
+
+    fb_distanciaFrente.on("value", function(snapshot) {
+      var valDF = snapshot.val();
+
+      if(valDF == 0) {
+        distanciaFrente.innerHTML = "NA";
+      } else {
+        distanciaFrente.innerHTML = valDF + " cm";
+      }
+    });
+
+    fb_distanciaAtras.on("value", function(snapshot) {
+      var valDA = snapshot.val();
+
+      if(valDA == 0) {
+        distanciaAtras.innerHTML = "NA";
+      } else {
+        distanciaAtras.innerHTML = valDA + " cm";
+      }
+    });
+
+    fb_temperatura.on("value", function(snapshot) {
+      var valTemp = snapshot.val();
+
+      if(valTemp < 60) {
+        temperatura.innerHTML = "Frio";
+      } else if( valTemp >= 60 && valTemp < 70) {
+        temperatura.innerHTML = "Templado";
+      }else{
+        temperatura.innerHTML = "Caliente";
+      }
+    });
+
+    // fb_movimiento.on("value", function(snapshot) {
+    //   if (snapshot.val() == 1) {
+    //     movimiento.innerHTML = "Hay Movimiento";
+    //     $("#cardM")
+    //       .addClass("bg-info")
+    //       .addClass("text-white");
+    //   } else {
+    //     movimiento.innerHTML = "No hay Movimiento";
+    //     $("#cardM")
+    //       .removeClass("bg-info")
+    //       .removeClass("text-white");
+    //   }
+    // });
   } else {
     // Cuando no se ha hecho login, solo muestra esta sección
     $("#divControles").addClass("collapse");
